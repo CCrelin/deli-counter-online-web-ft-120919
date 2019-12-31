272ea88bab60f22bd1b1fbe43c88500d42bf78ca
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-12-31 20:54:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,8 @@
 def line(customer)
-  if customer.length <= 0 
+  if customer.length < 0 
     puts "The line is currently empty."
+  elsif customer.length > 0 do 
+    customer.each_with_index |i, index|
+    puts 
   end
 end
