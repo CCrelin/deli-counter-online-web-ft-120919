11942cb692e15f10b3d9c45e72ce0bcad383685c
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-12-31 21:09:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(customer)
-  if customer.length < 0 
+  if customer.length == 0 
     puts "The line is currently empty."
-  elsif customer.length > 1  
+  elsif customer.length >= 1  
     counter = 1 
     puts "The line is currently:#{counter + "."}#{customer}"
     counter += 1
