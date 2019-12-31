b8cf0dc85722ba37408bd1f654c35865abcda859
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-12-31 20:56:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,6 @@ def line(customer)
     puts "The line is currently empty."
   elsif customer.length > 0 do 
     customer.each_with_index |i, index|
-    puts 
+    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
   end
 end
